b38fde10cd00f229da2ef802c13d1f61376d96fa
Brojulius <juliusvormelker@googlemail.com>
2024-07-15 15:39:18 +0200

    Create build.js

---
+++ build.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const apiKey = process.env.GOOGLE_API_KEY;
+
+const startseitePath = path.join(__dirname, 'Startseite.html');
+const questionPath = path.join(__dirname, 'question.html');
+
+const replaceApiKey = (filePath) => {
+    const content = fs.readFileSync(filePath, 'utf-8');
+    const updatedContent = content.replace('YOUR_GOOGLE_API_KEY', apiKey);
+    fs.writeFileSync(filePath, updatedContent);
+};
+
+replaceApiKey(startseitePath);
+replaceApiKey(questionPath);
