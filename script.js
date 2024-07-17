73b077949ecc14b9798630a1cb4d51165d4662bd
Brojulius <juliusvormelker@googlemail.com>
2024-07-17 13:42:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Google Sheets API initialisieren
+function initClient() {
+    gapi.client.init({
+        apiKey: 'AIzaSyAU8dBB-H49p1YHV0Y5_01AXZiO2gjATFE',
+        discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    }).then(() => {
+        console.log('GAPI client loaded for API');
+    }, (error) => {
+        console.error('Error loading GAPI client for API', error);
+    });
+}
+
 function startGame() {
     const groupName = document.getElementById('group-name').value;
     const direction = document.getElementById('direction').value;
@@ -5,21 +17,40 @@ function startGame() {
     if (groupName) {
         localStorage.setItem('groupName', groupName);
         localStorage.setItem('direction', direction);
+        saveGroupData(groupName, direction);
         window.location.href = 'question.html';
     } else {
         alert('Bitte geben Sie einen Gruppennamen ein.');
     }
 }
 
+function saveGroupData(groupName, direction) {
+    gapi.client.sheets.spreadsheets.values.append({
+        spreadsheetId: '1LIzHl56mbSOb6HW30PDNAve_ONXTfYkLuDlL13p-Nwc',
+        range: 'Blatt1!A1:B1',
+        valueInputOption: 'RAW',
+        insertDataOption: 'INSERT_ROWS',
+        resource: {
+            values: [
+                [groupName, direction]
+            ],
+        },
+    }).then((response) => {
+        console.log('Group data saved:', response);
+    }, (error) => {
+        console.error('Error saving group data:', error);
+    });
+}
+
 function loadQuestions() {
     gapi.client.sheets.spreadsheets.values.get({
         spreadsheetId: '1LIzHl56mbSOb6HW30PDNAve_ONXTfYkLuDlL13p-Nwc',
         range: 'HSU Campusrallye!A2:J10',
-    }).then(function(response) {
+    }).then((response) => {
         questionsData = response.result.values;
         displayQuestion();
         startTimer();
-    }, function(response) {
+    }, (response) => {
         console.log('Error: ' + response.result.error.message);
     });
 }
@@ -84,7 +115,7 @@ function setupSheets() {
     alert('Google Sheets eingerichtet');
 }
 
-// Google API initialization
+// Google API initialisieren
 gapi.load('client', () => {
     gapi.client.init({
         apiKey: 'AIzaSyAU8dBB-H49p1YHV0Y5_01AXZiO2gjATFE',
