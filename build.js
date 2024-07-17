cff64e09553f23cd7e1fe80f658ca8e45ce81935
Brojulius <Brojulius@users.noreply.github.com>
2024-07-17 11:25:02 +0000

    deploy: dcc131b329126dd5d017d74140df2d764ef8e573

---
+++ build.js
@@ -0,0 +1,19 @@
+// build.js
+const fs = require('fs');
+const path = require('path');
+
+const apiKey = process.env.GOOGLE_API_KEY;
+
+const questionPath = path.join(__dirname, 'question.html');
+const indexPath = path.join(__dirname, 'index.html');
+const scriptPath = path.join(__dirname, 'script.js');
+
+const replaceApiKey = (filePath) => {
+    const content = fs.readFileSync(filePath, 'utf-8');
+    const updatedContent = content.replace('YOUR_GOOGLE_API_KEY', apiKey);
+    fs.writeFileSync(filePath, updatedContent);
+};
+
+replaceApiKey(questionPath);
+replaceApiKey(indexPath);
+replaceApiKey(scriptPath);
